7f6cb3935dbdb63ef4981145af66260016820a90
Mrinal <upretimrinal@gmail.com>
2026-04-01 22:03:56 +0530

    initial project setup and responsive navbar

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-import Navbar from "./components/navbar.js";
-import Dashboard from "./components/dashboard.js";
-
-const navRoot = document.getElementById("navbar-root");
-const appRoot = document.getElementById("app-root");
-
-Navbar(navRoot);
-Dashboard(appRoot);
+console.log("App initialized");
