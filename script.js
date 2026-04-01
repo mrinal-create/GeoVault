69eda676a30ee6a23c4294478abaaf0beed7a749
Mrinal <upretimrinal@gmail.com>
2026-04-01 21:44:23 +0530

    Cleaned project: removed node_modules, public, and src folders

---
+++ script.js
@@ -0,0 +1,8 @@
+import Navbar from "./components/navbar.js";
+import Dashboard from "./components/dashboard.js";
+
+const navRoot = document.getElementById("navbar-root");
+const appRoot = document.getElementById("app-root");
+
+Navbar(navRoot);
+Dashboard(appRoot);
